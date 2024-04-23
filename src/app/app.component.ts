94701ca55c95228ef1cb5b5c611b9f72a2e4f516
Arashi-Crow <ignaciofdce92@gmail.com>
2024-04-23 15:14:15 -0600

    done dice app angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,21 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dice-app';
+  leftDice = '../assets/dice1.png';
+  rightDice = '../assets/dice2.png';
+
+  throwDice(): void {
+    const number1 = this.randomIntFromInterval(1, 6);
+    const number2 = this.randomIntFromInterval(1, 6);
+    this.leftDice = `../assets/dice${number1}.png`;
+    this.rightDice = `../assets/dice${number2}.png`;
+  }
+
+  randomIntFromInterval(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }
